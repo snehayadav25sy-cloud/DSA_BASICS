e0834cfbb06335c9198d95a85477c853dfd015e4
sneha yadav <snehayadav25.sy@gmail.com>
2026-04-12 22:15:52 +0530

    Update Pattern01.java

--- src/patterns/Pattern01.java
+++ src/patterns/Pattern01.java
@@ -5,7 +5,7 @@ public class Pattern01 {
     static void print01(int n){
        for(int i =0; i<n ; i++){
            for(int j =0 ; j<n ; j++){
-               System.out.print("*");
+               System.out.print("*");  
            }
            System.out.println();
 
