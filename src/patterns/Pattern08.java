f1dfbec14110e9a21c86057367ba9e6271d6727b
Sneha Yadav <snehayadav25@gmail.com>
2026-04-12 16:40:20 +0530

    third pattern

---
+++ src/patterns/Pattern08.java
@@ -0,0 +1,30 @@
+package patterns;
+import java.util.Scanner;
+
+
+public class Pattern08 {
+    static void print08(int n) {
+        for (int i = 0; i < n; i++) {
+            // space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*n-(2*i+1) ; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i ; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print08(n);
+
+    }
+}
