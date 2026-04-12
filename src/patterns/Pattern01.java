c4c9bb27678e19ccfd1c395c659390cbf4a98542
sneha yadav <snehayadav25.sy@gmail.com>
2026-04-12 22:19:17 +0530

    Update Pattern01.java

--- src/patterns/Pattern01.java
+++ src/patterns/Pattern01.java
@@ -14,7 +14,7 @@ public class Pattern01 {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         Scanner sc = new Scanner(System.in);  
-        int n = sc.nextInt();
+        int n = sc.nextInt();  
         print01(n);
 
     }
