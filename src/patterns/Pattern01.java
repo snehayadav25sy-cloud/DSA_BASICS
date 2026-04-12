a3cf04cd3ea52d67f55755d9aa721938bbb644bb
Sneha Yadav <snehayadav25@gmail.com>
2026-04-12 16:14:02 +0530

    first pattern

---
+++ src/patterns/Pattern01.java
@@ -0,0 +1,22 @@
+package patterns;
+import java.util.Scanner;
+
+public class Pattern01 {
+    static void print01(int n){
+       for(int i =0; i<n ; i++){
+           for(int j =0 ; j<n ; j++){
+               System.out.print("*");
+           }
+           System.out.println();
+
+       }
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print01(n);
+
+    }
+}
+
