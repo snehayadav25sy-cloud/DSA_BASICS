a06e38acd50e8beda761bd41939dcff7e1c50e37
sneha yadav <snehayadav25.sy@gmail.com>
2026-04-14 23:16:22 +0530

    Update rec02.java

--- src/Recursions/rec02.java
+++ src/Recursions/rec02.java
@@ -10,7 +10,7 @@ public class rec02 {
         System.out.println(i + " ");
         Rec01(i+1 , n );
 
-    }
+    }  
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
