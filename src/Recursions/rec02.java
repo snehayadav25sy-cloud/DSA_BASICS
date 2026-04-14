eee5f12910e732a93794595b26197ac851174a54
Sneha Yadav <snehayadav25@gmail.com>
2026-04-14 23:11:36 +0530

    print from 1 to n

---
+++ src/Recursions/rec02.java
@@ -0,0 +1,19 @@
+package Recursions;
+
+import java.util.Scanner;
+
+public class rec02 {
+    static void Rec01(int i , int n){
+        if(i>n){
+            return;
+        }
+        System.out.println(i + " ");
+        Rec01(i+1 , n );
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Rec01(1,n);
+    }
+}
